cc50d0d2c249873065542fd4824f6d68e3b01ebd
Sombaran Gupta <sombaran.gupta@yahoo.com>
2024-01-06 21:43:10 +0530

    240106A

--- design-pattern/singleton_design.cpp
+++ design-pattern/singleton_design.cpp
@@ -6,6 +6,10 @@
 
 
 #include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx;
 
 class Singleton {
 protected:
@@ -19,23 +23,47 @@ private:
 public:
 	Singleton (const Singleton& other) = delete;
 	void operator = (const Singleton& other) = delete;
-	static Singleton* getInstance (uint value);
+	static Singleton* getInstance (uint value) {
+		std::lock_guard<std::mutex> gLock (mtx);
+		if (nullptr == mInstance) {
+			mInstance = new (std::nothrow)Singleton(value);
+		}
+		return mInstance;
+	}
+
+	static void deleteInstance () {
+		if (mInstance) {
+			delete mInstance;
+			mInstance = nullptr;
+		}
+	}
+
+	void printAddress() {
+		std::cout << "My instance memory address is:" << this << std::endl;
+	}
 };
 
+void printCalling () {
+	std::cout << "Hi, I am a thread ID:[" << std::this_thread::get_id() << "]" << std::endl;
+	//Singleton::getInstance(4200)->printAddress();
+	Singleton::getInstance(1356)->printAddress();
+
+}
 
 Singleton* Singleton::mInstance {nullptr};
 
-Singleton* Singleton::getInstance(uint value) {
-	if (nullptr == mInstance) {
-		mInstance = new Singleton(value);
-	}
-	return mInstance;
-}
-
 int main() {
-	Singleton* p = Singleton::getInstance(4200); 
-	Singleton* s = Singleton::getInstance(1356); //even we create many objects only one and the top object is instantiated
-	std::cout << "Address of s" << &s << std::endl;
-	std::cout << "Address of p" << &p << std::endl;
+	//Singleton* p = Singleton::getInstance(4200); 
+	//Singleton* s = Singleton::getInstance(1356); //even we create many objects only one and the top object is instantiated
+	//std::cout << "Address of s" << &s << std::endl;
+	//std::cout << "Address of p" << &p << std::endl;
+
+	std::thread t1(printCalling);
+	std::thread t2(printCalling);
+
+	if (t1.joinable() and t2.joinable()) {
+		t1.join(); t2.join();
+	}
+	Singleton::deleteInstance();
 	return (0);
 }
---
+++ design-pattern/solid_principle.cpp
@@ -0,0 +1,100 @@
+/*
+@Brief: SOLID Principle
+S --> A class should have only one reason to change 
+      Single responsiblity principle
+O --> A class should be open for extension but closed for modificatino. 
+      OCP-->adding new functionality without modifying the existing code.
+      Open-closed principle
+L --> If B is sub-type of class A, then we should be able to replace the object 
+      of A with B without breaking the behavior of the program in other words,
+      a subclass should be able to be substituted for its parent class without 
+      causing any errors or unexpected behavior.
+      Liskov substitution principle
+I --> Interface should be designed in such a way that client should not implement
+      unneccesary methods which is not required.
+      Interface segregation principle
+D --> high-level modules should not depend on low-level modules
+      Both should depend on abstractions.
+      This principle promotes loose coupling and allows for easier changes and substitutions
+      Dependency inversion principle
+*/
+#include <iostream>
+#include <memory>
+
+//Single responsiblity principle
+class Marker {
+    std::string m_color;
+    std::string m_name;
+    int m_year;
+    int m_price;
+    public:
+
+    Marker (std::string color, std::string name, int year,int price) 
+    : m_color (color)
+    , m_name (name)
+    , m_year (year)
+    , m_price (price) {
+        std::cout << "I am parameterized constructor \n";
+        std::cout << this->m_color << std::endl;
+        std::cout << this->m_name << std::endl;
+        std::cout << this->m_year << std::endl;
+        std::cout << this->m_price << std::endl;
+    }
+
+    ~Marker() {
+        std::cout << "I am destructor \n";
+    }
+};
+
+class Invoice {
+    Marker *_marker;
+    int m_quantity;
+    public:
+    Invoice (Marker *marker, int quantity)
+    : _marker (marker)
+    , m_quantity (quantity) {
+        std::cout << "I am parameterized constructor \n";
+    }
+    /*
+    ~Invoice () {
+        std::cout << "I am destructor \n";
+    }*/
+
+    int calculatePrice (const Marker& obj) {
+        //int price = ((marker.m_price) * (this->m_quantity));
+        //return price;
+
+    }
+};
+
+int main1() {
+    std::shared_ptr <Marker>Obj = std::make_shared<Marker>("Blue","Parker", 2007, 100);
+    //Marker mObj("Purple","Parker", 2008, 102);
+    //Invoice invObj;
+    //invObj.calculatePrice(mObj);
+    Marker * mObj = new Marker("Purple","Parker", 2008, 102);
+    //Invoice inObj(mObj);
+}
+//Liskov substitution principle
+class Bird {
+    public:
+    virtual void fly()=0;
+    virtual ~Bird() = default;
+};
+
+class Eagle : public Bird {
+    public:
+    void fly() override {
+        std::cout << __func__ << std::endl;
+    }
+};
+
+void makeBirdFly(Bird& bird) {
+  bird.fly();
+}
+int main2() {
+    //std::shared_ptr<Bird>eagle = std::make_shared<Eagle>();
+    Eagle eagle;
+    makeBirdFly(eagle); // okay, eagle can fly
+}
+
