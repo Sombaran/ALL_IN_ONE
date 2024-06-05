f80ea10e4d8c85385d75982fd800f1b7d81047d2
Sombaran Gupta <sombaran.gupta@yahoo.com>
2024-06-05 14:21:09 +0530

    242303B:thread join

--- multi-threading/join-thread.cpp
+++ multi-threading/join-thread.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <thread>
 #include <algorithm>
+#include <vector>
+
 class WorkerThread
 {
 public:
@@ -16,14 +18,19 @@ int main()
     std::vector<std::thread> threadList;
     for (int i = 0; i < 10; i++)
     {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
         threadList.push_back(std::thread(WorkerThread()));
     }
     // Now wait for all the worker thread to finish i.e.
     // Call join() function on each of the std::thread object
-
     std::cout << "wait for all the worker thread to finish" << std::endl;
 
-    std::for_each(threadList.begin(), threadList.end(), [](std::thread& t1) {t1.join();});
+    std::for_each(threadList.begin(), threadList.end(), [](std::thread& t1) {
+        if(t1.joinable()) 
+        {
+            t1.join();
+        }
+        });
 
     std::cout << "Exiting from Main Thread" << std::endl;
     return 0;
